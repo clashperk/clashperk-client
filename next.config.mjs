678690a2fc154c579c2db183f82f8fe75a36b9a7
suvajit <suvajit.me@gmail.com>
2024-10-25 21:08:15 +0530

    feat: setup redirect rules

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,31 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: false
+  reactStrictMode: false,
+  async redirects() {
+    return [
+      {
+        source: '/invite',
+        destination:
+          'https://discord.com/oauth2/authorize?client_id=526971716711350273',
+        permanent: true
+      },
+      {
+        source: '/discord',
+        destination: 'https://discord.gg/ppuppun',
+        permanent: true
+      },
+      {
+        source: '/faq',
+        destination: 'https://docs.clashperk.com/faq',
+        permanent: true
+      },
+      {
+        source: '/guide',
+        destination: 'https://docs.clashperk.com/overview/getting-set-up',
+        permanent: true
+      }
+    ];
+  }
 };
 
 export default nextConfig;
